f496bb837d6ae3b6c8e048d3d383fc35030eeda8
tuguito04 <jefri2sosaduran@gmail.com>
2025-08-26 15:11:06 -0400

    feat: start migration from front-js to React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Importamos React, la librería principal para construir interfaces de usuario
+import React from 'react';
+// Importamos ReactDOM para renderizar nuestra app en el DOM del navegador
+import ReactDOM from 'react-dom/client';
+// Importamos BrowserRouter para habilitar el enrutamiento en nuestra aplicación
+import {BrowserRouter} from 'react-router-dom';
+// Importamos el componente principal de la aplicación
+import App from './App.jsx';
+// Importamos los estilos CSS globales de la aplicación
+import "./assets/styles.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Buscamos el elemento con id 'root' en nuestro HTML y creamos un root de React
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // React StrictMode activa comprobaciones adicionales en desarrollo
+  <React.StrictMode>
+    {/* BrowserRouter envuelve la app para habilitar rutas */}
+    <BrowserRouter>
+      {/* Componente principal de la aplicación */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
